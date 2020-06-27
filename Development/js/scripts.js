f700fff1d5ac32af08e9c49b3343e007b5a57dd3
gttffv <66701766+gttffv@users.noreply.github.com>
2020-06-27 20:08:34 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,38 @@
 // السؤال الأول
 // 1
-
+const name = ("احمد");
 // 2
-
+ Number =(17);
 // 3 and 4
-
+console.log("انا" + " " + name + " "+ "وعمري" + " " + Number);
 // 5 and 6
-
+console.log(Number=5)
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbys = ["football","swimming"];
+console.log(hoppys);
 // 2
-
+hobbys.push(17);
+console.log(hoppys);
 // 3
-
+huppys.pop()
+console.log(hubbys)
 // 4
-
+let chico ={
+    language: css
+};
 // 5
-
+ let a =["moudhi", "hassan", "rahaf" , "nancy"];
 // 6
-
+chico = {
+ language = "css",
+ name: "web",
+ a = ["moudhi", "hassan", "rahaf" , "nancy"]
+};
 // 7
-
+  console.log(hassan)
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
